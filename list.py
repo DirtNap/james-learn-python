65fdaa5a9814192509ccaa13bcbb47051e1a4ec8
Michael Donnelly <MichaelVDonnelly@gmail.com>
2020-06-01 20:26:56 -0400

    Learning about lists

---
+++ list.py
@@ -0,0 +1,12 @@
+my_list = list(range(100))
+other_list = ["James", "Baby Monkey", 2006, True]
+
+
+def print_list(words, l):
+    print("value of ", words)
+    print("size:", len(l))
+    print(l)
+
+
+print_list("My list", my_list)
+print_list("Other list", other_list)
